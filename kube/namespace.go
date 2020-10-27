6c8c038db380c57ce462c92790a87dd1050181fa
parvez <parvez@yellowmessenger.com>
2020-10-27 22:07:51 +0530

    added namespace client

--- kube/namespace.go
+++ kube/namespace.go
@@ -5,9 +5,19 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func (c *KubeClientSet) GetNamespace(ns string) (*v12.Namespace, error) {
+	client := c.GetNamespaceClient()
+	return client.Get(c.Ctx, ns, v1.GetOptions{})
+}
+
+func (c *KubeClientSet) CreateNamespace(ns *v12.Namespace, opts v1.CreateOptions) (*v12.Namespace, error) {
+	client := c.GetNamespaceClient()
+	return client.Create(c.Ctx, ns, opts)
+}
+
 func (c *KubeClientSet) NamespaceList(opts v1.ListOptions) (*v12.NamespaceList, error) {
 	nsClient := c.GetNamespaceClient()
-	list, err := nsClient.List(c.Ctx, v1.ListOptions{})
+	list, err := nsClient.List(c.Ctx, opts)
 	return list, err
 }
 
